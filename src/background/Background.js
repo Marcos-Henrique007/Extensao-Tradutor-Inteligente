eb71a1508fa1b95fe0e1fbef797b3b2f482f070a
Marcos-Henrique007 <marcoshenriqueufrn@gmail.com>
2026-05-25 21:15:35 -0300

    Tentando ajeitar o erro

--- src/background/Background.js
+++ src/background/Background.js
@@ -1,16 +1,31 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "translate",
-        title: "Traduzir texto selecionado",
-        contexts: ["selection"]
-    });
-});
+// proteção total contra undefined
+if (chrome.contextMenus) {
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "translate") {
-        chrome.tabs.sendMessage(tab.id, {
-            type: "TRANSLATE",
-            text: info.selectionText
+    chrome.runtime.onInstalled.addListener(() => {
+        chrome.contextMenus.create({
+            id: "translate",
+            title: "Traduzir texto selecionado",
+            contexts: ["selection"]
         });
-    }
-});
+    });
+
+    chrome.contextMenus.onClicked.addListener((info, tab) => {
+        if (info.menuItemId === "translate") {
+
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                files: ["src/content/content.js"]
+            }, () => {
+
+                chrome.tabs.sendMessage(tab.id, {
+                    type: "TRANSLATE",
+                    text: info.selectionText
+                });
+
+            });
+        }
+    });
+
+} else {
+    console.error("contextMenus NÃO disponível");
+}
